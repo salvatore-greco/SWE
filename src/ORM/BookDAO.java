e91b96a5a474cb63961956c479f17b3cf9112756
StefanoUnifi <stefano.marianelli@edu.unifi.it>
2026-03-01 17:39:20 +0100

    Created functions that: add books to the db and to get a list of all the books in the db.

--- src/ORM/BookDAO.java
+++ src/ORM/BookDAO.java
@@ -26,9 +26,39 @@ public class BookDAO {
                 throw new BookNotFoundException("Book with code " + code + " not found");
         }
         catch (SQLException e){
-            e.printStackTrace();
-            return null; //FIXME same as in UserDAO
+            throw new RuntimeException("Database error while retrieving book", e);
+            //TODO: valutare se trasformarla in un eccezione deicata del DAO layer (metterla nel package Exception.data)
         }
-
     }
+
+    public void saveBook(BookDTO book) throws SQLException {
+        Connection conn = ConnectionManager.getInstance().getConnection();
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO book (code, isbn, title) VALUES (?, ?, ?)");
+
+        stmt.setString(1, book.getCode());
+        stmt.setString(2, book.getIsbn());
+        stmt.setString(3, book.getTitle());
+        stmt.executeUpdate();
+    }
+
+    public List<BookDTO> getAllBooks()  {
+        List<BookDTO> books = new ArrayList<>();
+
+        try{
+            Connection conn = ConnectionManager.getInstance().getConnection();
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM book");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                books.add(new BookDTO(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3)
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+    //TODO: valutare se aggiungere altri metodi in generale (tipo update o delete anche se potrebbero non essere necessari)
 }
