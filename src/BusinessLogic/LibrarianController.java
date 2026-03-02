4a9c88b3e9993c6cda63c28c815389f88a6b3842
StefanoUnifi <stefano.marianelli@edu.unifi.it>
2026-03-02 18:20:43 +0100

    Implemented feature (setEvent) to add an event in the db. Also implemented in LibrarianController functions that allows a Librarian to create a card and an event.

--- src/BusinessLogic/LibrarianController.java
+++ src/BusinessLogic/LibrarianController.java
@@ -1,11 +1,33 @@
 package BusinessLogic;
 
-import DomainModel.Librarian;
-import DomainModel.User;
+import DomainModel.*;
+import ORM.CardDAO;
+import ORM.EventDAO;
 
 public class LibrarianController implements ControllerInterface{
-    private Librarian user;
+    private Librarian librarian;
+    private CardDAO cardDAO = new CardDAO();
+    private EventDAO eventDAO = new EventDAO();
+
     public LibrarianController(User user){
         this.user = (Librarian) user;
+    }
+
+    public Card createCard(LibraryUser user){
+        Integer cardId = cardDAO.setRequestedCard(user);
+        if (cardId != null)
+            return new Card(cardId);
+        else
+            throw new RuntimeException("Write on database failed");
+    }
+
+    public Event createEvent(Event event){
+        Integer id = eventDAO.setEvent(event);
+        if (id != null) {
+            event.setId(id);
+            return event;
+        } else
+            throw new RuntimeException("Write on database failed");
+
     }
 }
