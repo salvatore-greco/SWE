ab135176724600a0c1890cff0ad2e12d2a1332a3
salvatore.greco <greco.salvatore03@gmail.com>
2026-03-02 18:57:44 +0100

    fix: fixed setEvent to be compatible with interval database's type

--- src/ORM/EventDAO.java
+++ src/ORM/EventDAO.java
@@ -2,9 +2,11 @@ package ORM;
 
 import DomainModel.Event;
 import DomainModel.LibraryUser;
+import org.postgresql.util.PGInterval;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EventDAO {
@@ -38,11 +40,12 @@ public class EventDAO {
     public Integer setEvent(Event event) {
         try {
             Connection conn = ConnectionManager.getInstance().getConnection();
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO event (name, description, date, duration, organizer, place) values (?, ?, ?, ?, ?, ?)");
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO event (name, description, date, duration, organizer, room) values (?, ?, ?, ?, ?, ?)");
             stmt.setString(1, event.getName());
             stmt.setString(2, event.getDescription());
-            stmt.setTimestamp(3, java.sql.Timestamp.valueOf(event.getDate()));
-            stmt.setLong(4, event.getDuration());
+            stmt.setTimestamp(3, java.sql.Timestamp.valueOf(event.getStartDate()));
+            //Postgre è compatibile con il to string di duration per gli oggetti di tipo interval
+            stmt.setString(4, event.getEventDuration().toString());
             stmt.setString(5, event.getOrganizer().getEmail());
             stmt.setInt(6, event.getPlace().getNumber());
 
