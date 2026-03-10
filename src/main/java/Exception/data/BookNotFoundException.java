f80bdd014942628fe76c44ca71809ace5a134bc8
salvatore.greco <greco.salvatore03@gmail.com>
2026-03-10 16:04:37 +0100

    info: made custom exception unchecked

--- src/main/java/Exception/data/BookNotFoundException.java
+++ src/main/java/Exception/data/BookNotFoundException.java
@@ -1,6 +1,6 @@
 package Exception.data;
 
-public class BookNotFoundException extends Exception{
+public class BookNotFoundException extends RuntimeException{
     public BookNotFoundException(String message){
         super(message);
     }
