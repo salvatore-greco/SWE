fc810e808861d96ac30780f53a59dc5e84a9c3ae
salvatore.greco <greco.salvatore03@gmail.com>
2026-03-12 16:43:51 +0100

    feat: test: added unit test for event room

--- src/test/java/DomainModel/EventRoomUnitTest.java
+++ src/test/java/DomainModel/EventRoomUnitTest.java
@@ -12,23 +12,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class EventRoomUnitTest {
 
-   /* @BeforeEach
-    public void init(){
-        EventRoom aValidEventRoom = new EventRoom(0, 50);
-        ArrayList<Event> scheduledEvents = new ArrayList<>();
-        scheduledEvents.add(new Event.EventBuilder()
+    private EventRoom eventRoom;
+    private Event eventA;
+
+    @BeforeEach
+    public void init() {
+        eventRoom = new EventRoom(0, 50);
+
+        eventA = new Event.EventBuilder()
                 .setName("Nome")
-                .setPlace(aValidEventRoom)
+                .setPlace(eventRoom)
                 .setDescription("lorem ipsum")
                 .setStartDate(LocalDateTime.of(2026, 3, 1, 10, 0))
                 .setEventDuration(Duration.of(1, ChronoUnit.HOURS))
-                .setOrganizer(new Librarian("mario","prova@mail.com", "rossi"))
-                .build()
-        );
-    }*/
+                .setOrganizer(new Librarian("mario", "prova@mail.com", "rossi"))
+                .build();
+    }
 
     @Test
-    public void testEventRoomCommonConstructor() {
+    public void test_EventRoom_CommonConstructor() {
         EventRoom aValidEventRoom = new EventRoom(0, 50);
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
             new EventRoom(1, -10);
@@ -37,26 +39,27 @@ public class EventRoomUnitTest {
     }
 
     @Test
-    public void testInvalidTimeIntervalThrowsException() {
-        EventRoom aValidEventRoom = new EventRoom(0, 50);
-
+    public void test_EventRoom_isAvailableInvalidTimeInterval_ThrowsException() {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
-           aValidEventRoom.isAvailable(null, Duration.of(10, ChronoUnit.SECONDS));
+            eventRoom.isAvailable(null, Duration.of(10, ChronoUnit.SECONDS));
         });
         assertEquals("Invalid time interval", e.getMessage()); //Falliscono sempre dal solito if, lo controllo una volta sola
         assertThrows(IllegalArgumentException.class, () -> {
-            aValidEventRoom.isAvailable(LocalDateTime.of(2026, 3, 1, 10, 0),null);
+            eventRoom.isAvailable(LocalDateTime.of(2026, 3, 1, 10, 0), null);
         });
-//        Event eventA = new Event.EventBuilder()
-//                .setName("Nome")
-//                .setPlace(aValidEventRoom)
-//                .setDescription("lorem ipsum")
-//                .setStartDate(LocalDateTime.of(2026, 3, 1, 10, 0))
-//                .setEventDuration(Duration.of(1, ChronoUnit.HOURS))
-//                .setOrganizer(new Librarian("mario", "prova@mail.com", "rossi"))
-//                .build();
-//        aValidEventRoom.scheduleEvent(eventA);
+    }
 
+    @Test
+    public void test_EventRoom_schedulingValidEvent_doesNotThrow() {
+        assertDoesNotThrow(() -> eventRoom.scheduleEvent(eventA));
+    }
 
+    @Test
+    public void test_EventRoom_schedulingInvalidEvent_throws(){
+        eventRoom.scheduleEvent(eventA); //Inserisco un evento valido (funziona per via del precedente test)
+
+        //overlapping event. Rimetto il precedente
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> eventRoom.scheduleEvent(eventA));
+        assertEquals("Room is not available", e.getMessage());
     }
 }
