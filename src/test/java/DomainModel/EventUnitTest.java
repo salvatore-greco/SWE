c48025b139c3a439241e8a80173c090f05e63034
StefanoUnifi <stefano.marianelli@edu.unifi.it>
2026-03-13 18:38:40 +0100

    Implemented dates/durations/overlapping tests in EventUnitTest.

--- src/test/java/DomainModel/EventUnitTest.java
+++ src/test/java/DomainModel/EventUnitTest.java
@@ -39,4 +39,52 @@ public class EventUnitTest {
         assertEquals(eventRoomA, event.getPlace(), "Room mismatch");
         assertEquals(organizerA, event.getOrganizer(), "Organizer mismatch");
     }
+
+    @Test
+    public void test_Event_missingStartDate_throws(){
+        assertThrows(IllegalStateException.class, ()->{
+            new Event.EventBuilder()
+                    .setName("Nome")
+                    .setPlace(eventRoomA)
+                    .setDescription("lorem ipsum")
+                    .setEventDuration(Duration.of(1, ChronoUnit.HOURS))
+                    .setOrganizer(organizerA)
+                    .build();
+                });
+    }
+
+    @Test
+    public void test_Event_invalidDuration_throws(){
+        assertThrows(IllegalStateException.class, ()->{
+            new Event.EventBuilder()
+                    .setName("Nome")
+                    .setPlace(eventRoomA)
+                    .setDescription("lorem ipsum")
+                    .setStartDate(LocalDateTime.of(2026, 3, 1, 10, 0))
+                    .setEventDuration(Duration.of(-1, ChronoUnit.HOURS))
+                    .setOrganizer(organizerA)
+                    .build();
+        });
+    }
+
+    @Test
+    public void test_Event_getEndDate(){
+        LocalDateTime excpectedEndDate = LocalDateTime.of(2026, 3, 1, 11, 0);
+        assertEquals(excpectedEndDate, event.getEndDate());
+    }
+
+    @Test
+    public void test_Event_overlaps(){
+        Event eventB = new Event.EventBuilder()
+                .setName("Evento B")
+                .setPlace(eventRoomA)
+                .setDescription("lorem ipsum")
+                .setStartDate(LocalDateTime.of(2026, 3, 1, 10, 30))
+                .setEventDuration(Duration.of(1, ChronoUnit.HOURS))
+                .setOrganizer(organizerA)
+                .build();
+
+        assertTrue(event.overlaps(eventB));
+    }
+
 }
