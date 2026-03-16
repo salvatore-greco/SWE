d653ccee780735a101a92c0f439e4c9f09df3bba
StefanoUnifi <stefano.marianelli@edu.unifi.it>
2026-03-16 17:40:37 +0100

    Implemented generic room unit test.

---
+++ src/test/java/DomainModel/RoomUnitTest.java
@@ -0,0 +1,37 @@
+package DomainModel;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoomUnitTest {
+
+    static class TestRoom extends Room{
+        public TestRoom(int number, int seats) {
+            super(number, seats);
+        }
+
+        @Override
+        public boolean isAvailable(LocalDateTime start, Duration duration) {
+            return true;
+        }
+    }
+
+    @Test
+    public void test_Room_validRoomCreation(){
+        TestRoom room = new TestRoom(1, 20);
+
+        assertEquals(1, room.getNumber());
+        assertEquals(20, room.getSeats());
+    }
+
+    @Test
+    public void test_Room_invalidSeatsNumber(){
+        IllegalArgumentException e = assertThrows(
+                IllegalArgumentException.class, ()-> new TestRoom(1, -10));
+        assertEquals("Seats must be positive", e.getMessage());
+    }
+}
