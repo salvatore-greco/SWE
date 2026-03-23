b09469c5a799638de552a6d78f0a853b1a177569
salvatore.greco <greco.salvatore03@gmail.com>
2026-03-23 10:33:02 +0100

    feat: added get loan by book and card in loanDAO and test

--- src/main/java/ORM/LoanDAO.java
+++ src/main/java/ORM/LoanDAO.java
@@ -1,14 +1,17 @@
 package ORM;
 
+import DomainModel.Book;
+import DomainModel.Card;
 import DomainModel.Loan;
 
 import java.sql.*;
 import java.time.LocalDate;
 
 public class LoanDAO {
-    public LoanDAO(){}
+    public LoanDAO() {
+    }
 
-    public boolean setRequestedLoan(Loan loan){
+    public boolean setRequestedLoan(Loan loan) {
         try {
             Connection conn = ConnectionManager.getInstance().getConnection();
             PreparedStatement stmt = conn.prepareStatement("INSERT INTO loan(book, card, granted, ended) values(?, ?, ?, ?) ");
@@ -19,8 +22,7 @@ public class LoanDAO {
             int row = stmt.executeUpdate();
             stmt.close();
             return row > 0;
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
@@ -29,7 +31,7 @@ public class LoanDAO {
     public boolean isBookLoaned(String code) {
         try {
             Connection conn = ConnectionManager.getInstance().getConnection();
-            PreparedStatement stmt = conn.prepareStatement("SELECT 1 FROM loan WHERE book = ? AND ended = FALSE");
+            PreparedStatement stmt = conn.prepareStatement("SELECT 1 FROM loan WHERE book = ? AND ended = FALSE AND granted = TRUE");
             stmt.setString(1, code);
             return stmt.executeQuery().next();
         } catch (SQLException e) {
@@ -69,5 +71,38 @@ public class LoanDAO {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public Loan getLoanByBookAndCard(String code, int card) {
+        try {
+            String query = """
+                    SELECT *\s
+                    FROM loan AS l JOIN card AS c ON l.card = c.id JOIN book on book.code = l.book\s
+                    WHERE book = ? AND card = ?
+                   \s""";
+            Connection conn = ConnectionManager.getInstance().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, code);
+            stmt.setInt(2, card);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int cardId = rs.getInt("card");
+                String user = rs.getString("user");
+                LocalDate cardIssueDate = Utils.toLocalDateOrNull(rs.getDate("issuedate"));
+                LocalDate cardExpirationDate = Utils.toLocalDateOrNull(rs.getDate("expirationdate"));
+                boolean granted = rs.getBoolean("granted");
+                boolean ended = rs.getBoolean("ended");
+                String bookCode = rs.getString("book");
+                String title = rs.getString("title");
+                String isbn = rs.getString("isbn");
+                stmt.close();
+                return new Loan(new Card(cardId, cardIssueDate, cardExpirationDate),
+                        new Book.BookBuilder().setCode(bookCode).setISBN(isbn).setTitle(title).build(),
+                        granted, ended, cardIssueDate, cardExpirationDate);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/test/java/IntegrationTest/ORM/LoanDAOUnitTest.java
+++ src/test/java/IntegrationTest/ORM/LoanDAOUnitTest.java
@@ -10,14 +10,14 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LoanDAOUnitTest extends BaseDAOUnitTest{
+public class LoanDAOUnitTest extends BaseDAOUnitTest {
     private LoanDAO loanDAO = new LoanDAO();
 
     private BookDAO bookDAO = new BookDAO();
     private UserDAO userDAO = new UserDAO();
     private CardDAO cardDAO = new CardDAO();
 
-    private Book createBookTest(){
+    private Book createBookTest() {
         Book book = new Book.BookBuilder()
                 .setCode("A003")
                 .setISBN("1234567890125")
@@ -27,7 +27,7 @@ public class LoanDAOUnitTest extends BaseDAOUnitTest{
         return book;
     }
 
-    private Card createCardTest(){
+    private Card createCardTest() {
         UserDTO user = new UserDTO(
                 "prova3@email.com",
                 "UtenteTest",
@@ -48,7 +48,7 @@ public class LoanDAOUnitTest extends BaseDAOUnitTest{
         return card;
     }
 
-    private Loan createLoanTest(Book book, Card card){
+    private Loan createLoanTest(Book book, Card card) {
         return new Loan(card, book, false, false);
     }
 
@@ -61,23 +61,23 @@ public class LoanDAOUnitTest extends BaseDAOUnitTest{
         assertTrue(loanDAO.setRequestedLoan(loan));
     }
 
-     @Test
-     public void LoanDAO_isBookLoaned_returnsTrue() {
-         assertTrue(loanDAO.isBookLoaned("A001"));
-     }
+    @Test
+    public void LoanDAO_isBookLoaned_returnsTrue() {
+        assertTrue(loanDAO.isBookLoaned("A001"));
+    }
 
-     @Test
-     public void LoanDAO_isBookLoaned_bookNotOnLoan_returnsFalse() {
-         assertFalse(loanDAO.isBookLoaned("A002"));
-     }
+    @Test
+    public void LoanDAO_isBookLoaned_bookNotOnLoan_returnsFalse() {
+        assertFalse(loanDAO.isBookLoaned("A002"));
+    }
 
-      @Test
-      public void LoanDAO_isBookLoaned_bookNotExist_returnsFalse(){
-          assertFalse(loanDAO.isBookLoaned("B003"));
-      }
+    @Test
+    public void LoanDAO_isBookLoaned_bookNotExist_returnsFalse() {
+        assertFalse(loanDAO.isBookLoaned("B003"));
+    }
 
-     @Test
-    public void LoanDAO_grantLoan_returnsTrue(){
+    @Test
+    public void LoanDAO_grantLoan_returnsTrue() {
         Book book = createBookTest();
         Card card = createCardTest();
         Loan loan = createLoanTest(book, card);
@@ -87,9 +87,9 @@ public class LoanDAOUnitTest extends BaseDAOUnitTest{
         boolean result = loanDAO.grantLoan(loan, LocalDate.now(), LocalDate.now().plusDays(30));
 
         assertTrue(result);
-     }
+    }
 
-     @Test
+    @Test
     public void LoanDAO_grantLoan_loanNotGranted_returnsFalse() {
         Book book = createBookTest();
         Card card = createCardTest();
@@ -98,9 +98,9 @@ public class LoanDAOUnitTest extends BaseDAOUnitTest{
         boolean result = loanDAO.grantLoan(loan, LocalDate.now(), LocalDate.now().plusDays(30));
 
         assertFalse(result);
-     }
+    }
 
-     @Test
+    @Test
     public void LoanDAO_endLoan_existingLoan_returnsTrue() {
         Book book = createBookTest();
         Card card = createCardTest();
@@ -112,16 +112,22 @@ public class LoanDAOUnitTest extends BaseDAOUnitTest{
         boolean result = loanDAO.endLoan(loan);
 
         assertTrue(result);
-     }
+    }
 
-     @Test
-     public void LoanDAO_endLoan_notExistingLoan_returnsFalse(){
-            Book book = createBookTest();
-            Card card = createCardTest();
-            Loan loan = createLoanTest(book, card);
+    @Test
+    public void LoanDAO_endLoan_notExistingLoan_returnsFalse() {
+        Book book = createBookTest();
+        Card card = createCardTest();
+        Loan loan = createLoanTest(book, card);
 
-            boolean result = loanDAO.endLoan(loan);
+        boolean result = loanDAO.endLoan(loan);
 
-            assertFalse(result);
-     }
+        assertFalse(result);
+    }
+
+    @Test
+    public void LoanDAO_getLoanByBookAndCard_existingLoan_returnsLoan() {
+        Loan retrievedLoan = loanDAO.getLoanByBookAndCard("A001", 1);
+        assertNotNull(retrievedLoan);
+    }
 }
