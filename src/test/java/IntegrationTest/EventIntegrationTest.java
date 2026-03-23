2b74785ffe622cf43030366e5cd94d6dedc6261e
StefanoUnifi <stefano.marianelli@edu.unifi.it>
2026-03-23 18:34:39 +0100

    Implemented particular cases of create/cancel/modify event tests in EventIntegrationTest class. Modified private method createEventTest such that now verifies db integration.

--- src/test/java/IntegrationTest/EventIntegrationTest.java
+++ src/test/java/IntegrationTest/EventIntegrationTest.java
@@ -32,20 +32,15 @@ public class EventIntegrationTest extends BaseDAOUnitTest {
     }
 
     private Event createEventTest(){
-        EventRoom room = (EventRoom) roomDAO.getRoomByNumber(1);
-
-        Event event = new Event.EventBuilder()
-                .setName("EventoIntegrationTest")
-                .setDescription("Descrizione")
-                .setStartDate(LocalDateTime.now())
-                .setEventDuration(Duration.ofHours(2))
-                .setOrganizer(librarianController.getUser())
-                .setPlace(room)
-                .build();
-
-        Integer id = eventDAO.setEvent(event);
-        event.setId(id);
-        return event;
+        EventDTO event = new EventDTO(
+                LocalDateTime.now().plusHours(1),
+                Duration.ofHours(2),
+                "EventoIntegrationTest",
+                "Descrizione",
+                1,
+                librarianController.getUser().getEmail()
+        );
+        return librarianController.createEvent(event);
     }
 
     //test del librarian controller
@@ -56,10 +51,73 @@ public class EventIntegrationTest extends BaseDAOUnitTest {
     }
 
     @Test
+    public void createEvent_invalidRoom_throws(){
+        EventDTO event = new EventDTO(
+                LocalDateTime.now().plusHours(1),
+                Duration.ofHours(2),
+                "EventoIntegrationTest",
+                "Descrizione",
+                -1,
+                librarianController.getUser().getEmail()
+        );
+        var e = assertThrows(IllegalArgumentException.class, () -> librarianController.createEvent(event));
+        assertEquals("Room must exist", e.getMessage());
+    }
+
+    @Test
+    public void createEvent_pastDate_throws() {
+        EventDTO event = new EventDTO(
+                LocalDateTime.now().minusHours(1),
+                Duration.ofHours(1),
+                "Evento",
+                "Descrizione",
+                1,
+                librarianController.getUser().getEmail()
+        );
+        var e = assertThrows(IllegalArgumentException.class, () -> librarianController.createEvent(event));
+        assertEquals("Event start date cannot be in the past", e.getMessage());
+    }
+
+    @Test
+    public void createEvent_negativeDuration_throws() {
+        EventDTO event = new EventDTO(
+                LocalDateTime.now().plusHours(1),
+                Duration.ofHours(-1),
+                "Evento",
+                "Descrizione",
+                1,
+                librarianController.getUser().getEmail()
+        );
+        var e = assertThrows(IllegalArgumentException.class, () -> librarianController.createEvent(event));
+        assertEquals("Event duration must be positive", e.getMessage());
+    }
+
+    @Test
+    public void createEvent_StudyRoom_throws(){
+        EventDTO event = new EventDTO(
+                LocalDateTime.now().plusHours(1),
+                Duration.ofHours(1),
+                "Evento",
+                "Descrizione",
+                2, //la stanza 2 è una StudyRoom, riscontrare con data.sql
+                librarianController.getUser().getEmail()
+        );
+        var e = assertThrows(IllegalArgumentException.class, () -> librarianController.createEvent(event));
+        assertEquals("Room must be an Event Room", e.getMessage());
+    }
+
+    @Test
     public void cancelEvent(){
         Event eventToDelete = createEventTest();
-
         assertDoesNotThrow(() -> librarianController.cancelEvent(eventToDelete));
+    }
+
+    @Test
+    public void cancelEvent_notExisting_throws(){
+        Event eventToDelete = createEventTest();
+        eventToDelete.setId(-1);
+        var e = assertThrows(RuntimeException.class, () -> librarianController.cancelEvent(eventToDelete));
+        assertEquals("Event not found", e.getMessage());
     }
 
     @Test
@@ -70,5 +128,13 @@ public class EventIntegrationTest extends BaseDAOUnitTest {
         eventToModify.setDescription("Descrizione modificata");
 
         assertDoesNotThrow(() -> librarianController.modifyEvent(eventToModify));
+    }
+
+    @Test
+    public void modifyEvent_notExisting_throws(){
+        Event eventToModify = createEventTest();
+        eventToModify.setId(-1);
+        var e = assertThrows(RuntimeException.class, () -> librarianController.modifyEvent(eventToModify));
+        assertEquals("Event not found", e.getMessage());
     }
 }
